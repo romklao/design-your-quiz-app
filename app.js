1aa57dbf0ed5b2d7511635cb9f2c89dba3d14550
romklao <rk120879@gmail.com>
2017-01-18 23:20:42 -0800

    Initailized

---
+++ app.js
@@ -0,0 +1,20 @@
+
+
+
+// function start() {
+//     $('#startQuizz').click(function() {
+//         $('.firstPage').hide();
+//         $('.header').removeClass('styleQuizz');
+//         $('.firstQuestion').removeClass('styleQuizz');        
+//         $('.header').show();
+//         $('.answerQ1').show();
+//     })
+// }
+
+// function answerQ1() {
+
+// }
+
+// $(document).ready(function() {
+//     start();
+// })
